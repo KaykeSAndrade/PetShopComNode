33f1003cacb43f329a4f7e86bc65b795a356a242
Kayke SAndrade <kaykewwe1212@hotmail.com>
2022-06-28 09:12:24 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# PetShop em NodeJS
+Projeto PetShop feito em NodeJS
+
+### Pré-requisitos globais:
+
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para Rodar o Projeto
+
+`npm run start dev`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,57 @@
+import {Request, Response} from 'express'
+import { createMenuObjects } from '../helpers/createMenuObjects'
+import { Pet } from '../models/pet'
+
+export const home = (req:Request, res:Response) =>{
+
+    let list = Pet.getAll()
+
+    res.render("pages/pages",{
+        menu:createMenuObjects('all'),
+        banner: {
+            title:'Todos os animais',
+            background: 'allanimals.jpg'
+        },
+        list
+    })
+}
+
+export const dogs = (req:Request, res:Response) =>{
+    let list = Pet.getFromType('dog')
+
+    res.render("pages/pages",{
+        menu:createMenuObjects('dog'),
+           
+        banner:{
+            title:'Cachoros',
+            background: 'banner_dog.jpg'
+        },
+        list
+    })
+}
+
+export const cats = (req:Request, res: Response) =>{
+    let list = Pet.getFromType('cat')
+
+    res.render("pages/pages",{
+        menu:createMenuObjects('cat'),
+        banner:{
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        },
+        list
+    })
+}
+
+export const fishes = (req:Request,res:Response) =>{
+    let list = Pet.getFromType('fish')
+
+    res.render("pages/pages",{
+        menu:createMenuObjects('fish'),
+        banner:{
+            title:'Peixes',
+            background:'banner_fish.jpg'
+        },
+        list
+    })
+}
